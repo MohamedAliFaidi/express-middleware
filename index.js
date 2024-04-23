825add0e8a82034fb1dabbd38e888c1af6dbb3f7
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-04-23 14:01:42 +0100

    ejs

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.set('view engine', 'ejs');
+
 
 const test = require("./middleware")
 
@@ -14,6 +16,13 @@ app.get("/test",test, async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+
+app.get('/', (req, res) => {
+    // Render the 'index.ejs' template and pass data to it
+    res.render('index', { title: 'Hello World', message: 'Welcome to my website!' });
+  });
+  
 
 app.listen(4444, () => {
   console.log("server is up on 4444");
